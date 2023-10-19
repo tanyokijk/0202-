9b83aff0fcbf23d24c5b01c8058c56c610957561
Тетяна Деревянко <t.derevyanko1209@gmail.com>
2023-10-19 23:34:20 +0300

    Добавьте файлы проекта.

---
+++ 0202 один потік/0202 один потік.cpp
@@ -0,0 +1,24 @@
+﻿
+#include <iostream>
+#include <thread>
+
+void displayNumbers() 
+{
+    for (int i = 0; i <= 9; i++)
+    {
+        std::cout << i << std::endl;
+    }
+    for ( char i = L'a'; i <= L'f'; i++)
+    {
+        std::cout << i << std::endl;
+    }
+}
+
+int main()
+{
+    system("chcp 1251>null");
+    std::thread t(displayNumbers);
+    t.join();
+    std::cout<<"Головний потік закінчив виконання"<< std::endl;
+
+}
